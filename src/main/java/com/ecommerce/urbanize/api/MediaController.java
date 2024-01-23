3026ec5909bbeda97b69c4b278025acf02c9c8bb
ElPanita04 <lucgr04.vlc@gmail.com>
2024-01-23 20:39:35 +0100

    Arreglado cors

--- src/main/java/com/ecommerce/urbanize/api/MediaController.java
+++ src/main/java/com/ecommerce/urbanize/api/MediaController.java
@@ -7,21 +7,27 @@ import java.nio.file.Files;
 import java.util.Map;
 
 import org.springframework.core.io.Resource;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.HttpHeaders;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
 import org.springframework.web.multipart.MultipartFile;
+
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.ecommerce.urbanize.service.StorageService;
 
 import jakarta.servlet.http.HttpServletRequest;
 
+@CrossOrigin(origins = "*", allowedHeaders = "*", maxAge = 3600)
 @RestController
 @RequestMapping("/media")
 @AllArgsConstructor
