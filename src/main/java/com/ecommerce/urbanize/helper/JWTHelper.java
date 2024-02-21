ef643dc8ee70f7d9e45e17eae36452d7ac1ab00d
ElPanita04 <lucgr04.vlc@gmail.com>
2024-02-21 19:30:15 +0100

    Update JWT expiry time

--- src/main/java/com/ecommerce/urbanize/helper/JWTHelper.java
+++ src/main/java/com/ecommerce/urbanize/helper/JWTHelper.java
@@ -26,7 +26,7 @@ public class JWTHelper {
     public static String generateJWT(String username) {
 
         Date currentTime = Date.from(Instant.now());
-        Date expiryTime = Date.from(Instant.now().plus(Duration.ofSeconds(1500)));
+        Date expiryTime = Date.from(Instant.now().plus(Duration.ofSeconds(3600)));
 
         return Jwts.builder()
                 .setId(UUID.randomUUID().toString())
